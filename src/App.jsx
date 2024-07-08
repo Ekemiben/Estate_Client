236f09e5b0bf3070c48c377fc09af5abea114553
Ekemiben <ekemiben.4@gmail.com>
2024-07-08 19:54:54 +0100

    Create header componnent

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Home from "./Pages/Home"
 import SignIn from "./Pages/SignIn"
 import Profile from "./Pages/Profile"
 import SignUp from "./Pages/SignUp"
+import Header from "./components/Header/Header"
+import About from "./Pages/About"
 
 const App = () => {
   return<BrowserRouter>
+  <Header />
   <Routes>
     <Route path="/" element={<Home />}/>
     <Route path="/sign-in" element={<SignIn />}/>
     <Route path="/profile" element={<Profile />}/>
     <Route path="/sign-up" element={<SignUp />}/>
     <Route path="/sign-in" element={<SignIn />}/>
+    <Route path="/about" element={<About />}/>
   </Routes>
   </BrowserRouter>
 }
