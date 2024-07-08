4e49d0a5905c23fd89d8059b98143c0d5b2c023d
Ekemiben <ekemiben.4@gmail.com>
2024-07-08 17:07:49 +0100

    Installation Tailwind CSS, Vite, React and configuration completed

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-400 font-mono mr-11'>Welcome to our app</h1>
+    </div>
+  )
+}
+
+export default App
