62b8718b051cab3d5722f0fb60db6b7c67de3e92
Ekemiben <ekemiben.4@gmail.com>
2024-07-28 19:52:04 +0100

    Create and add OAuth component

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -7,9 +7,9 @@
 // exports.signup = async(req, res, next)=>{
 //     const {username, email, password} = req.body
 //     const hashPassword = bcrypt.hashSync(password, 10)
-//     const newUser = new User({username, email, password:hashPassword})
+//     const user = new User({username, email, password:hashPassword})
 //     try{
-//         await newUser.save()
+//         await user.save()
 //         res.status(201).json("user created successfully")
 //     }catch(error){
 //         next(error) 
@@ -55,9 +55,9 @@ import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
+    const user = new User({ username, email, password: hashedPassword });
     try {
-      await newUser.save();
+      await user.save();
       res.status(201).json('User created successfully!');
     } catch (error) {
       next(error);
@@ -71,7 +71,7 @@ export const signup = async (req, res, next) => {
       if (!validUser) return next(errorHandler(404, 'User not found!'));
       const validPassword = bcryptjs.compareSync(password, validUser.password);
       if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
-      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRETE);
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
       const { password: pass, ...rest } = validUser._doc;
       res
         .cookie('access_token', token, { httpOnly: true })
@@ -81,3 +81,33 @@ export const signup = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const google = async (req, res, next)=>{
+    try{
+      const user = await User.findOne({email:req.body.email});
+      if(user){
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET);
+        const {password: pass, ...rest} = user._doc;
+        console.log(rest)
+      
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+
+      }else{
+        const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+        const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+        const user = new User({username:req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password : hashedPassword, avatar: req.body.photo});
+        await user.save();
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET);
+        const {password: pass, ...rest} = user._doc;
+      
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+      }
+    }catch (error){
+      next(error)
+    }
+  }
+
+
+
+
+
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // import signin from '../controllers/auth.controller.js';
-import  {signin, signup }from '../controllers/auth.controller.js';
+import  {google, signin, signup }from '../controllers/auth.controller.js';
 // const Signin = require('../controllers/auth.controller.js')
 // const Signup = require('../controllers/auth.controller.js')
 
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.post("/signup", signup); 
 router.post("/signin", signin); 
+router.post("/google", google); 
 
 
 export default router;
