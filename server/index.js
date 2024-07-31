1149d68e0f03291dafda5d46d69c6f8d904c4953
Ekemiben <ekemiben.4@gmail.com>
2024-07-31 14:52:15 +0100

    User update api created

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import mongoose from 'mongoose';
 import cors from "cors"
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js'
+// import userRouter from './routes/user.route.js'
 // import authRouter from './controllers/auth.controller.js';
+import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.route.js';
+import userRouter from './routes/user.route.js';
 const app = express();
 dotenv.config()
 app.use(cors());
@@ -20,6 +22,8 @@ mongoose.connect(process.env.DB_CONNECTION)
 // const app = express();
 app.use(express.json())
 
+app.use(cookieParser());
+
 app.listen(5000, ()=>{
     console.log("Server is running on port 5000")
 })
@@ -28,7 +32,7 @@ app.listen(5000, ()=>{
 
 // app.use("/server/user",userRouter)
 app.use("/server/auth", authRouter)
-// app.use('/api/auth', authRouter);
+app.use("/server/user", userRouter)
 // Middleware to handle error
 app.use((err,req, res, next)=>{
     const statusCode = err.statusCode || 500;
